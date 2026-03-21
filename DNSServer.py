2eb9e0d31bd6727ec646ab53526c741edc89f44c
Shian Cerisier <soc2025@nyu.edu>
2026-03-21 19:15:32 -0400

    Serverside

--- DNSServer.py
+++ DNSServer.py
@@ -17,7 +17,8 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
-import ast
+
+
 
 def generate_aes_key(password, salt):
     kdf = PBKDF2HMAC(
@@ -31,24 +32,25 @@ def generate_aes_key(password, salt):
     return key
 
 # Lookup details on fernet in the cryptography.io documentation    
-def encrypt_with_aes(input_string, password, salt):
-    key = generate_aes_key(????)
-    f = Fernet(???)
-    encrypted_data = f.????(????.encode('utf-8')) #call the Fernet encrypt method
+def encrypt_with_aes(input_string, qpassword, salt):
+    key = generate_aes_key(password, salt)
+    f = Fernet(key)
+    encrypted_data = f.encrypt(input_string.encode('utf-8')) #call the Fernet encrypt method
     return encrypted_data    
 
 def decrypt_with_aes(encrypted_data, password, salt):
-    key = generate_aes_key(????)
-    f = Fernet(????)
-    decrypted_data = f.????(????) #call the Fernet decrypt method
+    key = generate_aes_key(password, salt)
+    f = Fernet(key)
+    decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
-salt = ???? # Remember it should be a byte-object
-password = ?????
-input_string = ?????
+salt = b'Tandon' # Remember it should be a byte-object
+password = "soc2025@nyu.edu"
+input_string = "AlwaysWatching -"
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
+encrypted_value_str = encrypted_value.decode('utf-8')
 
 # For future use    
 def generate_sha256_hash(input_string):
@@ -75,26 +77,113 @@ dns_records = {
             86400, #minimum
         ),
     },
-   
-    # Add more records as needed (see assignment instructions!
+    'safebank.com.': {
+        dns.rdatatype.A: '192.168.1.102',
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
+        dns.rdatatype.MX: [(10, 'mail.safebank.com.')],  # List of (preference, mail server) tuples
+        dns.rdatatype.CNAME: 'www.safebank.com.',
+        dns.rdatatype.NS: 'ns.safebank.com.',
+        dns.rdatatype.TXT: ('This is a TXT record',),
+        dns.rdatatype.SOA: (
+            'ns1.safebank.com.',  # mname
+            'admin.safebank.com.',  # rname
+            2023051000,  # serial
+            28800,  # refresh
+            7200,  # retry
+            604800,  # expire
+            600,  # minimum
+        ),
+    },
+
+    'google.com.': {
+        dns.rdatatype.A: '192.168.1.103',
+        dns.rdatatype.AAAA:'2607:f8b0:4006:81a::200e',
+        dns.rdatatype.MX: [(10, 'mail.google.com.')],
+        dns.rdatatype.CNAME: 'www.google.com',
+        dns.rdatatype.NS: 'ns.google.com',
+        dns.rdatatype.TXT: ('This is a TXT record',),
+        dns.rdatatype.SOA: (
+            'ns1.google.com', #mname
+            'admin.google.com', #rname
+            886680267,  # serial
+            900,  # refresh
+            900,  # retry
+            1800,  # expire
+            60,  # minimum
+        ),
+    },
+    'legitsite.com.': {
+        dns.rdatatype.A: '192.168.1.104',
+        dns.rdatatype.AAAA: '2600:1f18:4ae:c605:f094:2a0a:4985:526e',
+        dns.rdatatype.MX: [(10, 'mail.legitsite.com.')],
+        dns.rdatatype.CNAME: 'www.legitsite.com',
+        dns.rdatatype.NS: 'nsg1.namebright.com',
+        dns.rdatatype.TXT: ('This is a TXT record',),
+        dns.rdatatype.SOA: (
+            'ns1.namebright.com',  # mname
+            'admin.namebright.com',  # rname
+            2026032101,  # serial
+            28800,  # refresh
+            5000,  # retry
+            1209600,  # expire
+            10800,  # minimum
+        ),
+    },
+      'yahoo.com.': {
+        dns.rdatatype.A: '192.168.1.105',
+        dns.rdatatype.AAAA: '2001:4998:24:120d::1:1',
+        dns.rdatatype.MX: [(10, 'mta7.am0.yahoodns.net.')],
+        dns.rdatatype.CNAME: 'www.yahoo.com',
+        dns.rdatatype.NS: 'ns1.yahoo.com.',
+        dns.rdatatype.TXT: ('This is a TXT record',),
+        dns.rdatatype.SOA: (
+            'ns1.yahoo.com',  # mname
+            'admin.yahoo.com',  # rname
+            2026032001,  # serial
+            3600,  # refresh
+            300,  # retry
+            1814400,  # expire
+            600,  # minimum
+        ),
+      },
+       'nyu.edu.': {
+        dns.rdatatype.A: '192.168.1.106',
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com')],
+        dns.rdatatype.CNAME: 'www.nyu.edu',
+        dns.rdatatype.NS: 'ns4.nyu.net',
+        dns.rdatatype.TXT:  (encrypted_value_str,),
+        dns.rdatatype.SOA: (
+            'ns1.nyu.net.',  # mname
+            'hostmaster.nyu.edu',  # rname
+            2019014197,  # serial
+            10800,  # refresh
+            3600,  # retry
+            2419200,  # expire
+            900,  # minimum
+
+        )
+       }
+
 }
+
 
 def run_dns_server():
     # Create a UDP socket and bind it to the local IP address (what unique IP address is used here, similar to webserver lab) and port (the standard port for DNS)
-    server_socket = socket.socket(socket.AF_INET, ????) # Research this
-    server_socket.bind((?????, ????))
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Research this
+    server_socket.bind(('127.0.0.1', 8053))
 
     while True:
         try:
             # Wait for incoming DNS requests
             data, addr = server_socket.recvfrom(1024)
             # Parse the request using the `dns.message.from_wire` method
-            request = ?????
+            request = dns.message.from_wire(data)
             # Create a response message using the `dns.message.make_response` method
-            response = ??????
+            response = dns.message.make_response(request)
 
             # Get the question from the request
-            question = request.question[???]
+            question = request.question[0]
             qname = question.name.to_text()
             qtype = question.rdtype
 
@@ -105,12 +194,12 @@ def run_dns_server():
 
                 rdata_list = []
 
-                if qtype == dns.rdatatype.??:
+                if qtype == dns.rdatatype.MX:
                     for pref, server in answer_data:
                         rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
-                elif qtype == dns.rdatatype.??:
-                    ??, ??, ??, ??, ??, ??, ?? = answer_data # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
-                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, ??, ??, ??, ??, ??, ??, ??) # follow format from previous line
+                elif qtype == dns.rdatatype.SOA:
+                    mname, rname, serial, refresh, retry, expire, minimum = answer_data # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
+                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname,rname,serial, refresh, retry, expire, minimum) # follow format from previous line
                     rdata_list.append(rdata)
                 else:
                     if isinstance(answer_data, str):
@@ -126,7 +215,7 @@ def run_dns_server():
 
             # Send the response back to the client using the `server_socket.sendto` method and put the response to_wire(), return to the addr you received from
             print("Responding to request:", qname)
-            server_socket.??????? 
+            server_socket.sendto(response.to_wire(), addr)
         except KeyboardInterrupt:
             print('\nExiting...')
             server_socket.close()
@@ -152,5 +241,5 @@ def run_dns_server_user():
 
 if __name__ == '__main__':
     run_dns_server_user()
-    #print("Encrypted Value:", encrypted_value)
-    #print("Decrypted Value:", decrypted_value)
+    print("Encrypted Value:", encrypted_value)
+    print("Decrypted Value:", decrypted_value)
