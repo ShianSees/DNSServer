b5e1eafbec962b4251d523024bc90be70bc86d48
ShianSees <soc2025@nyu.edu>
2026-03-21 21:44:12 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -154,10 +154,10 @@ dns_records = {
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
-        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com')],
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.CNAME: 'www.nyu.edu.',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
-        dns.rdatatype.TXT: (encrypted_value,),
+        dns.rdatatype.TXT: str(encrypted_value,),
         dns.rdatatype.SOA: (
             'ns1.nyu.edu.',  # mname
             'hostmaster.nyu.edu.',  # rname
@@ -246,5 +246,4 @@ def run_dns_server_user():
 
 if __name__ == '__main__':
     run_dns_server_user()
-    # print("Encrypted Value:", encrypted_value)
-    # print("Decrypted Value:", decrypted_value)
+    
